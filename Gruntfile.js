ba7bd9f3f04ffcc46e4d320d87421accd800bab8
atharvaa9 <APARTE@STEVENS.EDU>
2024-03-19 21:22:49 -0700

    Minor functionality fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,29 @@
-
-const config = {
-    sass: {
-      dist: {
-        options: {
-          outputStyle: 'compressed'
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        'dart-sass': {
+            dist: {
+                options: {
+                    sourceMap: true
+                },
+                files: {
+                    'dist/css/styles.css': 'src/scss/styles.scss'
+                }
+            }
         },
-        files: {
-          'dist/css/styles.css': 'src/scss/styles.scss'
-        }
-      }
-    },
-    copy: {
-      dist: {
-        files: [
-          {
-            expand: true,
-            cwd: 'src/js/',
-            src: ['script.js'],
-            dest: 'dist/js/'
-          },
-          {
-            expand: true,
-            cwd: 'src/components/',
-            src: ['image-box.html'],
-            dest: 'dist/'
-          }
-        ]
-      }
-    },
-    watch: {
-      sass: {
-        files: ['src/scss/**/*.scss'],
-        tasks: ['sass:dist']
-      },
-      js: {
-        files: ['src/js/**/*.js'],
-        tasks: ['copy:dist']
-      },
-      html: {
-        files: ['src/*.html', 'src/components/*.html'],
-        tasks: ['copy:dist']
-      }
-    }
-  };
-  
-  const { loadNpmTasks } = grunt;
-  
-  loadNpmTasks('grunt-contrib-sass');
-  loadNpmTasks('grunt-contrib-copy');
-  loadNpmTasks('grunt-contrib-watch');
-  
-  grunt.initConfig(config);
-  
-  grunt.registerTask('default', ['sass:dist', 'copy:dist', 'watch']);
+        watch: {
+            styles: {
+                files: ['src/scss/**/*.scss'],
+                tasks: ['dart-sass'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-dart-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['dart-sass', 'watch']);
+};
--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,48 @@
-const IMG_SELECTOR = '.image-box img';
-const lazyLoadInstance = new lazyLoadLib(IMG_SELECTOR);
-const selectedImages = [];
-
-document.addEventListener('change', (event) => {
-    const checkbox = event.target;
-
-    if (checkbox.matches('.image-box input[type="checkbox"]')) {
-        const imageBox = checkbox.closest('.image-box');
-
-        if (checkbox.checked) {
-            selectedImages.push(imageBox.dataset.id);  
-        } else {
-            const index = selectedImages.indexOf(imageBox.dataset.id);
-            selectedImages.splice(index, 1);
+document.addEventListener('DOMContentLoaded', function() {
+    var lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
+  
+    if ('IntersectionObserver' in window) {
+      let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.classList.remove('lazy');
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        });
+      });
+  
+      lazyImages.forEach(function(lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+      });
+    } else {
+        // Fallback for browsers that don't support IntersectionObserver
+        var lazyLoadFallback = function() {
+          lazyImages.forEach(function(lazyImage) {
+            if (isInViewport(lazyImage)) {
+              lazyImage.src = lazyImage.dataset.src;
+              lazyImage.classList.remove('lazy');
+            }
+          });
+        };
+      
+        window.addEventListener('scroll', lazyLoadFallback);
+        window.addEventListener('resize', lazyLoadFallback);
+      
+        // Function to check if an element is within the viewport
+        function isInViewport(element) {
+          var rect = element.getBoundingClientRect();
+          return (
+            rect.bottom >= 0 &&
+            rect.right >= 0 &&
+            rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+          );
         }
-    }
+      
+        // Initially call the fallback function to load images in viewport
+        lazyLoadFallback();
+      }
+      
 });
